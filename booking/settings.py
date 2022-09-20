83c7f19a420f0e5a8048d61c13fac6a43c49e6c5
6310682718 <6310682718@student.tu.ac.th>
2022-09-20 12:36:16 +0700

    update setting

--- booking/settings.py
+++ booking/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y%-qr$ean^d@7*_!mib*4%*kngu_t1unx(r&qr-f8ae3@m^-%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://protected-badlands-58339.herokuapp.com/']
 
